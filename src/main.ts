a4613f8c6dbcd2f6f6eac203fc08c212fbe75385
Parv khatri <khatriparv@gmail.com>
2020-07-11 21:30:04 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,8 @@ import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
-  console.log("Hii")
+  console.log("Hii");
+  console.log("Hello World");
   enableProdMode();
 }
 
